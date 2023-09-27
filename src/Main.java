e9f61582ebb1725cf9243c893a741b5b5a27a8c2
OneJackS <mrsykes89@mail.ru>
2023-09-27 22:41:14 +0300

    Closed Scanner Input

--- src/Main.java
+++ src/Main.java
@@ -51,5 +51,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         System.out.println(calc(input.nextLine()));
+        input.close();
     }
 }
